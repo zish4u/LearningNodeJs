524d66142e38330ca4578dedc20be8e9fea7acb2
zish <zish4u@gmail.com>
2020-11-03 23:07:08 +0530

    Synchronus file system completed.

--- challenge.js
+++ challenge.js
@@ -7,3 +7,15 @@ fs.appendFileSync('test.txt', ". This line is appended in existing text.")
 //Read file without buffer
 const d = fs.readFileSync('test.txt', "utf-8")
 console.log(d)
+
+//Rename a file
+fs.renameSync('test.txt', 'renamed.txt')
+
+//To delete a file.
+fs.unlinkSync('renamed.txt')
+
+//To delete a folder...
+//Firstly created a folder.
+//fs.mkdirSync('new')
+//Now delete 'new' folder
+fs.rmdirSync('new')
