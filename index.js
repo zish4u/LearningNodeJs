b16e5beda28d66b9d377213d89645f00969efda9
zish <zish4u@gmail.com>
2020-11-03 09:59:50 +0530

    Learning file system module.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("Hello world")
+
+// Learning node modules
+const fs = require("fs")
+
+fs.writeFileSync('read.txt', "This file is created by file system module in node, during module learning.")
