1ecc68b6accabe7c789c61655b833dcdfa417037
zish <zish4u@gmail.com>
2020-11-03 18:45:51 +0530

    File rename method added.

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ fs.writeFileSync('read.txt', "This file is created by file system module in node
 //If file not exist then it will create a new file otherwise it will modify and overwrite the file of given name.
 fs.writeFileSync('read.txt', "Text updated.")
 
+
+//This method will append text in given file name.
 fs.appendFileSync('read.txt', "This line is added by append, appendFileSynch does not overwrite.")
 
 
@@ -20,3 +22,6 @@ console.log(bufferFormat)
 //Converting buffer into text
 const textData = bufferFormat.toString()
 console.log(textData)
+
+//To rename file
+fs.renameSync('read.txt', 'readWrite.txt')
