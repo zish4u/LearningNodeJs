70dc9e446824d7043f6c749c1875f792b290b6ef
zish <zish4u@gmail.com>
2020-11-03 16:07:05 +0530

    Learned appendFileSynch

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ const fs = require("fs")
 
 fs.writeFileSync('read.txt', "This file is created by file system module in node, during module learning.")
 
-//If file not exist then it will create a new file otherwise it will modify the file of given name.
+//If file not exist then it will create a new file otherwise it will modify and overwrite the file of given name.
 fs.writeFileSync('read.txt', "Text updated.")
+
+fs.appendFileSync('read.txt', "This line is added by append, appendFileSynch does not overwrite.")
