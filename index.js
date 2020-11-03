124e90e938c429859cfadd29198cda84dacfe6ac
zish <zish4u@gmail.com>
2020-11-03 10:08:31 +0530

    Learning file system module.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log("Hello world")
 
-// Learning node modules
+// Learning node modules.
+// Creating a new file.
 const fs = require("fs")
 
 fs.writeFileSync('read.txt', "This file is created by file system module in node, during module learning.")
+
+//If file not exist then it will create a new file otherwise it will modify the file of given name.
+fs.writeFileSync('read.txt', "Text updated.")
